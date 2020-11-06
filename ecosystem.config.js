2d70cdd040925e1f2e6dfd769214582078afb888
MAC <mac@dvkien-2.local>
2020-11-06 16:07:32 +0700

    update eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'BE-EXCEL',
-      script: 'dist/main.js',
+      script: 'yarn start',
   
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       args: 'one two',
