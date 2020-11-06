be9addd7c8cfddb0985abd2cb172a4a0a54faa4c
MAC <mac@dvkien-2.local>
2020-11-06 11:20:21 +0700

    test jenkins

--- src/modules/info/info.interface.ts
+++ src/modules/info/info.interface.ts
@@ -1,11 +1,11 @@
 import { Document } from "mongoose";
 
 export class IInfo extends Document { 
-    readonly receiver: string
-    readonly sender: string
-    readonly subject: string
-    readonly date: string
-    readonly text: string
-    readonly attachments: Array<Object>
+    readonly receiver: string;
+    readonly sender: string;
+    readonly subject: string;
+    readonly date: string;
+    readonly text: string;
+    readonly attachments: Array<Object>;
 
 }
